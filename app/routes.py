4faacc55968a1df26ee65ea8565ecf6769e26098
deanbunn <deanbunn@gmail.com>
2020-01-17 13:40:32 -0800

    20200117_1340hrs

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,12 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello World!"
+    user = {'username': 'Dean'}
+    return render_template('index.html', title='Furasuko', user=user)
+
+
 
 
